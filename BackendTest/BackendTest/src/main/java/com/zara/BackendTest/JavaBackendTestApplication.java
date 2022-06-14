b0a6d21bb2ac94b9905deec75b4a79088b37b8e3
Augusto Consoli <augustoconsoli52@gmail.com>
2022-06-14 17:40:31 +0200

    initial commit

---
+++ BackendTest/BackendTest/src/main/java/com/zara/BackendTest/JavaBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.zara.BackendTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendTestApplication.class, args);
+	}
+
+}
